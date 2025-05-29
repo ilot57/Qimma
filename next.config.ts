c9b82fb226349518f19f0e4a96453eec1fbf1efd
Yacine LANDOLSI <yacine.landolsi@cegedim.com>
2025-05-29 16:59:59 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      'https://rfrjimdiwfjqhhzjcqbn.supabase.co',
+      'your-s3-bucket.s3.amazonaws.com',
+    ],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
